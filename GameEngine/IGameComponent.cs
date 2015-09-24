a96279bfdb8f9cb63c30056dfff057a97ab4dc65
nexus87 <nexxuz0@googlemail.com>
2015-09-24 16:21:34 +0200

    Added project for the game engine

---
+++ GameEngine/IGameComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine
+{
+    interface IGameComponent
+    {
+        void Start();
+        void Update();
+        void Stop();
+
+        void Pause();
+        void Resume();
+    }
+}
