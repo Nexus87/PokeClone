04a54574363264ad2cbf739a920aba19a7c1adb4
Nexus87 <nexxuz0@googlemail.com>
2015-10-26 23:18:21 +0100

    Added Gen1BattleRules

--- Base/Move.cs
+++ Base/Move.cs
@@ -3,11 +3,11 @@ using System.Runtime.Serialization;
 
 namespace Base
 {
-	enum DamageCategory {
-		Physical,
-		Special,
-		Status
-	}
+    public enum DamageCategory {
+        Physical,
+        Special,
+        Status
+    }
 
     public enum Target
     {
@@ -15,26 +15,28 @@ namespace Base
         Self
     }
 
-	[DataContract]
-	public class MoveData {
-		[DataMember]
-		public string Name { get; set; }
-		[DataMember]
-		public PokemonType PkmType { get; set; }
-		[DataMember]
-		public int? Damage { get; set; }
-		[DataMember]
-		public int? Accuracy { get; set; }
-		[DataMember]
-		public int PP { get; set; }
+    [DataContract]
+    public class MoveData {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public PokemonType PkmType { get; set; }
+        [DataMember]
+        public int? Damage { get; set; }
+        [DataMember]
+        public int? Accuracy { get; set; }
+        [DataMember]
+        public int PP { get; set; }
         [DataMember]
         public int Priority { get; set; }
         [DataMember]
         public Target TargetMode { get; set; }
-	}
+        [DataMember]
+        public DamageCategory DamageType { get; set; }
+    }
 
-	public class Move
-	{
+    public class Move
+    {
         public override string ToString()
         {
             return Data.Name;
@@ -42,13 +44,13 @@ namespace Base
 
         public MoveData Data { get; private set; }
         public int RemainingPP { get; set; }
-		public Move (MoveData data)
-		{
+        public Move (MoveData data)
+        {
             if (data == null) throw new ArgumentNullException("data", "Argument should not be null");
 
             Data = data;
             RemainingPP = data.PP;
-		}
-	}
+        }
+    }
 }
 
---
+++ BattleLib/Gen1BattleRules.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Base;
+
+namespace BattleLib
+{
+    class Gen1BattleRules : IBattleRules
+    {
+        bool isTrainerBattle;
+
+        public event EventHandler<OnConditionChangedArgs> OnConditionChanged;
+        public event EventHandler<OnDamageTakenArgs> OnDamageTaken;
+        public event EventHandler<OnStatsChangedArgs> OnStatsChanged;
+
+        public Gen1BattleRules(bool isTrainerBattle)
+        {
+            this.isTrainerBattle = isTrainerBattle;
+        }
+        public bool CanChange()
+        {
+            return true;
+        }
+
+        public bool CanEscape()
+        {
+            return isTrainerBattle;
+        }
+
+        public bool ExecChange(Pokemon oldPkmn, Pokemon newPkmn)
+        {
+            return true;
+        }
+
+        public bool ExecMove(Pokemon source, Move move, Pokemon target)
+        {
+            switch (move.Data.DamageType)
+            {
+                case DamageCategory.Physical:
+                    HandlePhysicalDamage(source, move, target);
+                    break;
+                case DamageCategory.Special:
+                    HandleSpecialDamage(source, move, target);
+                    break;
+                case DamageCategory.Status:
+                    HandleStatusDamage(source, move, target);
+                    break;
+            }
+
+            return true;
+        }
+
+        private float CalculateDamage(int attack, int defense, Pokemon source, Move move)
+        {
+            float damage = (2.0f * source.Level + 10.0f) / 250.0f;
+            damage *= ((float)attack) / ((float)defense);
+            damage *= (float) move.Data.Damage;
+            damage += 2;
+
+            float modifier = move.Data.PkmType == source.Type1 || move.Data.PkmType == source.Type2 ?
+                1.5f : 1.0f;
+            // modifier *= type effectiveness
+            // modifier *= critical
+            // modifier *= random(0.85, 1.0)
+
+            return damage * modifier;
+        }
+        private void HandleSpecialDamage(Pokemon source, Move move, Pokemon target)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandleStatusDamage(Pokemon source, Move move, Pokemon target)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HandlePhysicalDamage(Pokemon source, Move move, Pokemon target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UseItem(Pokemon target, Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
