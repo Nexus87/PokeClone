d9defbbb6a616def2f1ba348a8a429159184ccac
Nexus87 <nexxuz0@googlemail.com>
2015-12-07 17:04:12 +0100

    Added tests for DefaultTableModel

---
+++ GameEngineTest/Views/DefaultTableModelTest.cs
@@ -0,0 +1,48 @@
+﻿using GameEngine.Graphics.Views;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngineTest.Views
+{
+    [TestFixture]
+    public class DefaultTableModelTest : IItemModelTest
+    {
+        DefaultTableModel<TestType> model;
+        [SetUp]
+        public void Setup()
+        {
+            model = new DefaultTableModel<TestType>();
+            testModel = model;
+        }
+
+        [TestCase]
+        public void SetDataTest()
+        {
+            var t1 = new TestType { testString = "T1" };
+            var t2 = new TestType { testString = "T2" };
+
+            Assert.AreEqual(1, model.Rows);
+            Assert.AreEqual(1, model.Columns);
+
+            Assert.IsTrue(model.SetData(t1, 1, 0));
+            Assert.AreEqual(2, model.Rows);
+            Assert.AreEqual(1, model.Columns);
+            Assert.AreEqual(t1, model.DataAt(1, 0));
+
+            Assert.IsTrue(model.SetData(t2, 1, 1));
+            Assert.AreEqual(2, model.Rows);
+            Assert.AreEqual(2, model.Columns);
+            Assert.AreEqual(t2, model.DataAt(1, 1));
+
+            Assert.IsTrue(model.SetData(t2, 2, 2));
+            Assert.AreEqual(3, model.Rows);
+            Assert.AreEqual(3, model.Columns);
+            Assert.AreEqual(t2, model.DataAt(2, 2));
+
+        }
+    }
+}
