6f9786fc26e078e6bdad18a3c56e3e756c65b9a0
Nexus87 <nexxuz0@googlemail.com>
2016-01-29 14:36:40 +0100

    Added new class "Container"

---
+++ GameEngine/Graphics/Basic/Container.cs
@@ -0,0 +1,38 @@
+﻿using GameEngine.Wrapper;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Graphics.Basic
+{
+    public class Container : AbstractGraphicComponent
+    {
+        protected List<IGraphicComponent> components = new List<IGraphicComponent>();
+
+        protected void AddComponent(IGraphicComponent comp)
+        {
+            components.Add(comp);
+        }
+
+        protected void RemoveComponent(IGraphicComponent comp)
+        {
+            components.Remove(comp);
+        }
+
+        public virtual override void Setup(ContentManager content)
+        {
+            foreach (var c in components)
+                c.Setup(content);
+        }
+
+        protected override void DrawComponent(GameTime time, ISpriteBatch batch)
+        {
+            foreach (var c in components)
+                c.Draw(time, batch);
+        }
+    }
+}
