1fe9d152ecf7c193b1dee617731f7a1ebfcddc3a
Nexus87 <nexxuz0@googlemail.com>
2016-02-09 16:59:38 +0100

    Changed SingleComponentLayout for new ILayout

--- GameEngine/Graphics/Layouts/SingleComponentLayout.cs
+++ GameEngine/Graphics/Layouts/SingleComponentLayout.cs
@@ -8,61 +8,18 @@ namespace GameEngine.Graphics.Layouts
 {
     public class SingleComponentLayout : AbstractLayout
     {
-        private IGraphicComponent component;
-
-        public override void AddComponent(IGraphicComponent component)
+        protected override void UpdateComponents(Container container)
         {
-            this.component = component;
-            Invalidate();
-        }
+            var components = container.Components;
 
-        public override void RemoveComponent(IGraphicComponent component)
-        {
-            if (this.component == component)
-                this.component = null;
-            // No need for invalidation here
-        }
-
-        public override void Setup(ContentManager content)
-        {
-            if (component != null)
-                component.Setup(content);
-        }
-
-        protected override void DrawComponents(GameTime time, ISpriteBatch batch)
-        {
-            if (component != null)
-                component.Draw(time, batch);
-        }
-
-        protected override void UpdateComponents()
-        {
-            if (component == null)
+            if (components.Count == 0)
                 return;
 
+            var component = components[0];
             component.X = X;
             component.Y = Y;
-
             component.Width = Width;
             component.Height = Height;
-        }
-
-        public override int Rows
-        {
-            get { return 1; }
-        }
-
-        public override int Columns
-        {
-            get { return 1; }
-        }
-
-        public override void LayoutContainer(Container container)
-        {
-            var comps = container.Components;
-            component = comps.Count == 0 ? null : comps[0];
-            Invalidate();
-            Update();
         }
     }
 }
--- GameEngineTest/Graphics/Layouts/SingleComponentLayoutTest.cs
+++ GameEngineTest/Graphics/Layouts/SingleComponentLayoutTest.cs
@@ -1,4 +1,5 @@
 ﻿using GameEngine.Graphics;
+using GameEngine.Graphics.Basic;
 using GameEngine.Graphics.Layouts;
 using Moq;
 using NUnit.Framework;
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameEngineTest.Util;
 
 namespace GameEngineTest.Graphics.Layouts
 {
@@ -14,14 +16,27 @@ namespace GameEngineTest.Graphics.Layouts
     public class SingleComponentLayoutTest : ILayoutTest
     {
         SingleComponentLayout layout;
+
         [SetUp]
         public void Setup()
         {
-            var componentMock = new GraphicComponentMock();
             layout = new SingleComponentLayout();
-            layout.AddComponent(componentMock);
 
             testLayout = layout;
+            testContainer = new Container(engineMock.Object);
+            testContainer.FillContainer(1);
+        }
+
+        [TestCase]
+        public void MultipleComponentsTest()
+        {
+            var batch = new SpriteBatchMock();
+            testContainer.FillContainer(10);
+            testLayout.LayoutContainer(testContainer);
+
+            testContainer.Draw(batch);
+            Assert.AreEqual(1, batch.Objects);
+            batch.Objects[0].IsInConstraints(testContainer);
         }
     }
 }
