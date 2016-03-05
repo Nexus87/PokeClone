d7053b72f7acc9604cbc0df7ea59becfdbbdbfa8
Nexus87 <nexxuz0@googlemail.com>
2016-03-05 20:58:23 +0100

    Added AnimatedTextureBox

---
+++ GameEngine/Graphics/Basic/AnimatedTextureBox.cs
@@ -0,0 +1,47 @@
+﻿using GameEngine.Wrapper;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace GameEngine.Graphics.Basic
+{
+    internal class AnimatedTextureBox : ForwardingGraphicComponent<TextureBox>
+    {
+        public readonly List<Texture2D> Textures = new List<Texture2D>();
+        private int currentIndex = 0;
+        private int seconds = 0;
+
+        public AnimatedTextureBox(PokeEngine game) : base(new TextureBox(game), game)
+        {
+            SecondsPerImage = 1;
+        }
+
+        public int SecondsPerImage { get; set; }
+
+        public override void Draw(GameTime time, ISpriteBatch batch)
+        {
+            seconds += time.ElapsedGameTime.Seconds;
+            if (seconds >= SecondsPerImage)
+            {
+                Invalidate();
+                seconds = 0;
+            }
+            base.Draw(time, batch);
+        }
+
+        public override void Setup(ContentManager content)
+        {
+            base.Setup(content);
+        }
+
+        protected override void Update()
+        {
+            if (Textures.Count == 0)
+                return;
+
+            currentIndex = (currentIndex + 1) % Textures.Count;
+            InnerComponent.Image = Textures[currentIndex];
+        }
+    }
+}
