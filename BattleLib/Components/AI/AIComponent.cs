00f7e5d8eda14c6ef852227afefa7db0993eb459
Nexus87 <nexxuz0@googlemail.com>
2016-03-08 17:27:03 +0100

    Added AIComponent + Cleanup

---
+++ BattleLib/Components/AI/AIComponent.cs
@@ -0,0 +1,19 @@
+﻿using GameEngine;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleLib.Components.AI
+{
+    class AIComponent : GameComponent
+    {
+        public AIComponent(PokeEngine game)
+            : base(game)
+        {
+
+        }
+    }
+}
