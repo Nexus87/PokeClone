86d6bdb09c9c05bc2a2a959d8015accc758588f5
Nexus87 <nexxuz0@googlemail.com>
2017-01-21 14:25:28 +0100

    add dummy test for travis ci

--- src/MainModeTest/Class1.cs
+++ src/MainModeTest/Class1.cs
@@ -1,7 +1,16 @@
-﻿namespace MainModeTest
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace MainModeTest
 {
+    [TestFixture()]
     public class Class1
     {
+        [Test]
+        public void Dummy()
+        {
+
+        }
 
     }
 }
