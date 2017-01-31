f1fd0a562dd33af397b9dd3452b7965c4b0cb9e8
Nexus87 <nexxuz0@googlemail.com>
2017-01-31 21:19:26 +0100

    Adding ScriptManager

---
+++ src/GameEngine.TypeRegistry/GameEngine.Scripting/ScriptFunction.cs
@@ -0,0 +1,20 @@
+﻿using MoonSharp.Interpreter;
+
+namespace GameEngine.Scripting
+{
+    public class ScriptFunction
+    {
+        private readonly string _functionName;
+        private readonly Script _script;
+
+        internal ScriptFunction(Script script, string functioName)
+        {
+            _script = script;
+            _functionName = functioName;
+        }
+        public DynValue Call(params object[] parameters)
+        {
+            return _script.Call(_script.Globals[_functionName], parameters);
+        }
+    }
+}
---
+++ src/GameEngine.TypeRegistry/GameEngine.Scripting/ScriptManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using MoonSharp.Interpreter;
+
+namespace GameEngine.Scripting
+{
+
+    public class ScriptManager
+    {
+        private readonly Dictionary<string, string> _scripts = new Dictionary<string, string>();
+
+        public void AddScriptFile(string path, string functionName)
+        {
+            _scripts[functionName] = path;
+        }
+
+        public ScriptFunction GetScriptFunction<T1, T2, T3>(string functionName)
+        {
+            var script = new Script();
+            script.DoFile(_scripts[functionName]);
+
+            return new ScriptFunction(script, functionName);
+        }
+    }
+}
