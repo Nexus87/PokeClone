8177feb7415aa008092571356dceeee2115e2e3b
Nexus87 <nexxuz0@googlemail.com>
2017-03-14 21:56:01 +0100

    fix debug build

--- src/GameEngine.GUI/Renderers/AbstractRenderer.cs
+++ src/GameEngine.GUI/Renderers/AbstractRenderer.cs
@@ -1,4 +1,5 @@
-﻿using GameEngine.Graphics.General;
+﻿using GameEngine.Graphics.Diagnostic;
+using GameEngine.Graphics.General;
 using GameEngine.Graphics.Textures;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
