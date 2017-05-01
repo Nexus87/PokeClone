31d34ae1911ebba3ca23be880ea6f2f4d1da3a8d
Nexus87 <nexxuz0@googlemail.com>
2017-05-01 14:34:06 +0200

    Added state interface

---
+++ src/GameEngine.Core/GameStates/AbstractState.cs
@@ -0,0 +1,21 @@
+﻿using GameEngine.ECS;
+
+namespace GameEngine.Core.GameStates
+{
+    public abstract class AbstractState : IState
+    {
+        protected readonly EntityManager EntityManager;
+        protected readonly SystemManager SystemManager;
+
+        protected AbstractState(EntityManager entityManager, SystemManager systemManager)
+        {
+            EntityManager = entityManager;
+            SystemManager = systemManager;
+        }
+
+        public abstract void Init();
+        public abstract void Pause();
+        public abstract void Resume();
+        public abstract void Stop();
+    }
+}
---
+++ src/GameEngine.Core/GameStates/IState.cs
@@ -0,0 +1,10 @@
+﻿namespace GameEngine.Core.GameStates
+{
+    public interface IState
+    {
+        void Init();
+        void Pause();
+        void Resume();
+        void Stop();
+    }
+}
